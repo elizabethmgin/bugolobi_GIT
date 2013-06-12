fe106e9d50edd02a4c87b4e8d84bc67c210a4555
elizabethmgin <elizabethmgin@gmail.com>
2013-06-11 19:11:46 -0700

    adding init

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from peewee import *
+from config import DATABASE
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+database = SqliteDatabase(DATABASE, threadlocals=True)
+database.connect()
+
+from app import views, models
